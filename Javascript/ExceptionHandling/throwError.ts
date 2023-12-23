be8ee9b9f44c37b40c337d3a351114338852abe6
Kai Cho <kai.cho@netlogix.co.nz>
2023-12-24 03:48:26 +1300

    testing deep error

--- Javascript/ExceptionHandling/throwError.ts
+++ Javascript/ExceptionHandling/throwError.ts
@@ -1,1 +1,32 @@
-console.log("test")
+/**
+ * Testing try and catch
+ */
+
+
+const errorHandling = () => {
+
+	try {
+		const error = throwError()
+		console.log(error);	
+	} catch (err) {
+		console.error(`Error-errorHandling function: ${err}`);
+		return err
+	}
+}
+
+const throwError = () => {
+	throw new Error("Make error");
+}
+
+const deepError = () => {
+	try {
+		const testDeepError = errorHandling()
+		console.log('testDeepError', testDeepError);
+	} catch (error) {
+		console.log(`Error-deepError function: ${error}`);
+	}
+}
+
+// errorHandling()
+
+deepError()
